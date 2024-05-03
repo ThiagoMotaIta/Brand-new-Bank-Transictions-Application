464faf11a1ff10279c3a47c51f01788508052a5c
ThiagoMotaIta <thiagomotaita1@gmail.com>
2024-05-03 15:24:54 -0300

    Adjusting some error handle

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -8,13 +8,18 @@ use App\Models\User;
 class UserService {
 
 	public function getAllUsersService() {
- 
-        $userList = User::get();
-
-        return response()->json([
-            "message" => "User list",
-            "users" => $userList,
-        ], 200);
+        try {
+            $userList = User::get();
+    
+            return response()->json([
+                "message" => "User list",
+                "users" => $userList,
+            ], 200);
+        } catch (Exception $e){
+            return response()->json([
+              'error' => $e->getMessage()
+            ], 405);
+        }
 
 	}
 
